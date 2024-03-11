1a8a92c049b6eb790783180d81109cf6fba4b0fa
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-03-11 00:12:37 -0700

    logs added

--- a_hello_world/my-app/src/main.ts
+++ a_hello_world/my-app/src/main.ts
@@ -1,12 +1,20 @@
+
+/*Bootstraps is  an instance of an Angular 
+application and renders a 
+standalone component as 
+the application's root component*/
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+
+
+/********Root component************/ 
+/*The root component passed into this function must be a
+standalone one (should have the standalone: true 
+  flag in the @Component decorator config */
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
   template: '<div>Hello world!!</div>',
 })
 class AppComponent {
