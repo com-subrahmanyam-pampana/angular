741c844496df02300c9550529e6319e0ab2be189
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-03-22 11:24:43 -0700

    directives added

--- g_directives/src/app/app.component.ts
+++ g_directives/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FirstComponent,SecondComponent,ThirdComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+
 }
