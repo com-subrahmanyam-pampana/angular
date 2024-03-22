d902d1dcbc5b57a762bd1d21bf6f9c7a4be0ce93
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-03-22 11:35:29 -0700

    directives added

--- g_directives/src/app/first/first.component.ts
+++ g_directives/src/app/first/first.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './first.component.html',
   styleUrl: './first.component.css'
 })
 export class FirstComponent {
+   isSpecial =false;
+   currentStyles: Record<string, string> = {};
+   constructor(){
+    this.currentStyles = {
+      'font-style': this.isSpecial ? 'italic' : 'normal',
+      'font-weight': !this.isSpecial ? 'bold' : 'normal',
+      'font-size': this.isSpecial ? '24px' : '12px',
+    };
 
+    setTimeout(()=>{this.isSpecial=true},10000)
+   }
+  
 }
