60ed25f085ec1afd9492f23c84f05d0523b06e21
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-03-22 10:52:14 -0700

    pipes added

--- pipes/src/app/first/first.component.ts
+++ pipes/src/app/first/first.component.ts
@@ -1,38 +1,23 @@
-import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [DatePipe,UpperCasePipe,AsyncPipe],
+  imports: [DatePipe, UpperCasePipe, AsyncPipe,JsonPipe],
   templateUrl: './first.component.html',
   styleUrl: './first.component.css'
 })
 export class FirstComponent {
   today: number = Date.now();
+  myObject: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
+  /*AsyncPipe*/
   greeting: Promise<string> | null = null;
-  arrived: boolean = false;
-
-  private resolve: Function | null = null;
-
-  constructor() {
-    this.reset();
-  }
-
-  reset() {
-    this.arrived = false;
-    this.greeting = new Promise<string>((resolve, reject) => {
-      this.resolve = resolve;
-    });
-  }
 
   clicked() {
-    if (this.arrived) {
-      this.reset();
-    } else {
-      this.resolve!('hi there!');
-      this.arrived = true;
-    }
+    this.greeting = new Promise<string>((resolve, reject) => {
+      resolve('hi there!')
+    });
   }
 
 }
